343679989040fc7069c549d61a432942ec9b2d47
Jae Kim <nanonerd@gmail.com>
2015-06-22 16:43:13 -0700

    first commit

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,3 +1,37 @@
 /**
  * Created by jkim on 6/22/2015.
+ * Name: LinkIt
+ * Version: 0.1.0
+ * License: MIT
  */
+
+(function($){
+    $.fn.linkIt = function(options){
+        // Default settings
+        var settings = $.extend({
+            href        : null,
+            text        : null,
+            target      : '_self'
+        }, options);
+
+        // Validate
+        if (settings.href == null){
+            console.log('you need an href option for linkIt to work');
+            return this;
+        }
+
+        return this.each(function(){
+            var object = $(this);
+
+            if (settings.text == null){
+                settings.text = object.text();
+            }
+
+            console.log('settings.target: ' + settings.target);
+            console.log('settings.href: ' + settings.href);
+            console.log('settings.text: ' + settings.text);
+
+            object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+        });
+    }
+}(jQuery));
